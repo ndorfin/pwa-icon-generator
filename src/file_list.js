d5cb340e7d3b9bfac6214da64424764e3777f32c
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-08-24 21:50:06 +1200

    Start defining a list of touch-icons

---
+++ src/file_list.js
@@ -0,0 +1,178 @@
+export default [
+  {
+    name: 'favicon.ico',
+    type: 'image/x-icon',
+    desc: 'Browser Title Icon',
+    width: 16,
+    height: 16
+  },
+  {
+    name: 'favicon.ico',
+    type: 'image/x-icon',
+    desc: 'Taskbar Shortcut Icon',
+    width: 32,
+    height: 32
+  },
+  {
+    name: 'apple-touch-icon-precomposed.png',
+    type: 'image/png',
+    width: 57,
+    height: 57
+  },
+  {
+    name: 'apple-touch-icon-57x57-precomposed.png',
+    type: 'image/png',
+    width: 57,
+    height: 57
+  },
+  {
+    name: 'apple-touch-icon-60x60-precomposed.png',
+    type: 'image/png',
+    width: 60,
+    height: 60
+  },
+  {
+    name: 'ms-touch-icon-70x70.png',
+    type: 'image/png',
+    platform: 'windows',
+    desc: 'Windows Metro UI Icon, Square',
+    width: 70,
+    height: 70
+  },
+  {
+    name: 'touch-icon-72x72.png',
+    type: 'image/png',
+    width: 72,
+    height: 72
+  },
+  {
+    name: 'apple-touch-icon-76x76-precomposed.png',
+    type: 'image/png',
+    width: 76,
+    height: 76
+  },
+  {
+    name: 'touch-icon-96x96.png',
+    type: 'image/png',
+    desc: 'Desktop Shortcut Icon & Google TV Icon',
+    width: 96,
+    height: 96
+  },
+  {
+    name: 'apple-touch-icon-114x114-precomposed.png',
+    type: 'image/png',
+    width: 114,
+    height: 114
+  },
+  {
+    name: 'apple-touch-icon-120x120-precomposed.png',
+    type: 'image/png',
+    platform: 'iOS',
+    browser: 'Safari',
+    desc: 'iPhone Retina, iOS 7+',
+    width: 120,
+    height: 120
+  },
+  {
+    name: 'touch-icon-128x128.png',
+    type: 'image/png',
+    platform: 'webstore',
+    desc: 'Chrome Webstore Icon',
+    width: 128,
+    height: 128
+  },
+  {
+    name: 'apple-touch-icon-144x144-precomposed.png',
+    type: 'image/png',
+    width: 144,
+    height: 144
+  },
+  {
+    name: 'apple-touch-icon-152x152-precomposed.png',
+    type: 'image/png',
+    platform: 'iOS',
+    browser: 'Safari',
+    desc: 'iPad Retina, iOS 7+',
+    width: 152,
+    height: 152
+  },
+  {
+    name: 'apple-touch-icon-167x167-precomposed.png',
+    type: 'image/png',
+    platform: 'iOS',
+    browser: 'Safari',
+    desc: 'iPad Pro, iOS 8+',
+    width: 167,
+    height: 167
+  },
+  {
+    name: 'apple-touch-icon.png',
+    type: 'image/png',
+    platform: 'iOS',
+    browser: 'Safari',
+    width: 180,
+    height: 180
+  },
+  {
+    name: 'apple-touch-icon-180x180-precomposed.png',
+    type: 'image/png',
+    platform: 'iOS',
+    browser: 'Safari',
+    desc: 'iPhone 6 Plus, iOS 8+',
+    width: 180,
+    height: 180
+  },
+  {
+    name: 'touch-icon-192x192.png',
+    type: 'image/png',
+    width: 192,
+    height: 192
+  },
+  {
+    name: 'touch-icon-196x196.png',
+    type: 'image/png',
+    browser: 'chrome',
+    platform: 'android',
+    width: 196,
+    height: 196
+  },
+  {
+    name: 'touch-icon-228x228.png',
+    type: 'image/png',
+    browser: 'opera',
+    desc: 'Opera Coast Icon',
+    width: 228,
+    height: 228
+  },
+  {
+    name: 'touch-icon-256x256.png',
+    type: 'image/png',
+    platform: 'android',
+    width: 256,
+    height: 256
+  },
+  {
+    name: 'ms-touch-icon-270x270.png',
+    type: 'image/png',
+    platform: 'windows',
+    desc: 'Windows Metro UI Icon, Square',
+    width: 270,
+    height: 270
+  },
+  {
+    name: 'ms-touch-icon-310x150.png',
+    type: 'image/png',
+    platform: 'windows',
+    desc: 'Windows Metro UI Icon, Landscape',
+    width: 310,
+    height: 150
+  },
+  {
+    name: 'ms-touch-icon-310x310.png',
+    type: 'image/png',
+    platform: 'windows',
+    desc: 'Windows Metro UI Icon, Square',
+    width: 310,
+    height: 310
+  }
+];
---
+++ src/shell_script.js
@@ -0,0 +1,3 @@
+import fileList from './file_list.js';
+
+// let output = `convert ${input} -resize ${width}x${height} ${name}`;
