0141852d3400fc1be38a0bb4c596f8a5225c37ae
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-10-21 21:37:41 +1300

    Add Readme. Update shell-script to use at least two source images.

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
-sourceImage="touch-icon-256x256.png"
-sizeArray="\
+sourceImageMedium="touch-icon-128x128.png"
+sizeArrayMedium="\
 57x57:apple-touch-icon-precomposed.png \
 57x57:apple-touch-icon-57x57-precomposed.png \
 72x72:apple-touch-icon-72x72-precomposed.png \
 96x96:touch-icon-96x96.png \
 114x114:apple-touch-icon-114x114-precomposed.png \
 120x120:apple-touch-icon-120x120-precomposed.png \
-128x128:touch-icon-128x128.png \
+"
+
+sourceImageLarge="touch-icon-256x256.png"
+sizeArrayLarge="\
 144x144:apple-touch-icon-144x144-precomposed.png \
 150x150:ms-touch-icon-150x150.png \
 152x152:apple-touch-icon-152x152-precomposed.png \
@@ -21,12 +24,18 @@ function getArguments {
 }
 
 function getOuterArray {
-  for size in $sizeArray
+  for size in $sourceArray
   do
     getArguments `echo $size | tr : " "`
   done
 }
 
+sourceImage="$sourceImageLarge"
+sourceArray="$sizeArrayLarge"
+getOuterArray
+
+sourceImage="$sourceImageMedium"
+sourceArray="$sizeArrayMedium"
 getOuterArray
 
 # 57 -> precomposed
